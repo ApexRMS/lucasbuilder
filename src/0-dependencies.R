184347a633df7238cb62d1aeaf1104ad8db5893b
AmandaSchwantes <152832879+AmandaSchwantes@users.noreply.github.com>
2025-09-05 15:42:34 -0400

    applied auto formatting

--- src/0-dependencies.R
+++ src/0-dependencies.R
@@ -4,14 +4,14 @@
 # Run with R-4.1.1
 # This script installs missing dependencies and loads all dependencies
 
-depend <- c("rsyncrosim", "RODBC", "tidyverse","imputeTS") 
+depend <- c("rsyncrosim", "RODBC", "tidyverse", "imputeTS")
 ndepend <- length(depend)
-present <- installed.packages()[ , "Package"]
-needed <- depend[!(depend %in% present)] 
+present <- installed.packages()[, "Package"]
+needed <- depend[!(depend %in% present)]
 nneeded <- length(needed)
-if(nneeded > 0){
+if (nneeded > 0) {
   install.packages(needed, repos = "https://cloud.r-project.org/")
 }
-for(dep in 1:ndepend){
+for (dep in 1:ndepend) {
   suppressMessages(eval(bquote(library(.(depend[dep])))))
 }
